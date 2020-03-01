4b2ccc725d213c9ee6fb99c9e06366bb19c87383
Adam Domaszewski <adamdox@gmail.com>
2020-03-01 14:27:32 +0100

    Add implementation

--- Cw1/Cw1/Program.cs
+++ Cw1/Cw1/Program.cs
@@ -1,11 +1,29 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace Cw1
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            var client = new HttpClient();
+            var result = await client.GetAsync("https://www.pja.edu.pl");
+
+            if (result.IsSuccessStatusCode)
+            {
+                var html = await result.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+                var matches = regex.Matches(html);
+                foreach (var mail in matches)
+                {
+                    Console.WriteLine(mail);
+                }
+            }
+
             Console.WriteLine("Hello World!");
         }
     }
